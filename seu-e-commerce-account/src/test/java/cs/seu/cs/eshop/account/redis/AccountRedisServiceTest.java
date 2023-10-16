dd8342e90dcd8a8bcc58a54c362cf77827e094e1
Shuxin Wang <81904920+A-BigTree@users.noreply.github.com>
2023-10-16 18:13:22 +0800

    Update AccountRedisServiceTest.java

--- seu-e-commerce-account/src/test/java/cs/seu/cs/eshop/account/redis/AccountRedisServiceTest.java
+++ seu-e-commerce-account/src/test/java/cs/seu/cs/eshop/account/redis/AccountRedisServiceTest.java
@@ -17,8 +17,11 @@ class AccountRedisServiceTest {
 
     @Test
     public void testRedis() {
-        accountRedisService.setValue("test", "账户缓存测试", accountRedisTest);
-        log.info(accountRedisService.getValue("test", accountRedisTest));
+        accountRedisService.setValue("test1", "账户缓存测试", accountRedisTest);
+        log.info(accountRedisTest.buildKey("test1"));
+        log.info("test1: {}", accountRedisService.getValue("test1", accountRedisTest));
+        accountRedisService.setObjectValue("test2", 1234L, accountRedisTest);
+        log.info("test2: {}", accountRedisService.getObjectValue("test2", accountRedisTest, Long.class));
     }
 
 }
