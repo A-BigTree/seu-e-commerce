16ea8d9c2318201cb1e77a364676d5fb936683eb
Shuxin Wang <81904920+A-BigTree@users.noreply.github.com>
2023-11-05 23:13:23 +0800

    add data crawling

--- seu-e-commerce-uni-app/src/utils/config.js
+++ seu-e-commerce-uni-app/src/utils/config.js
@@ -1,11 +1,11 @@
 // 测试
-const domain = "http://localhost:8086/eshop/api"; //统一接口域名，测试环境
+// const domain = "http://localhost:8086/eshop/api"; //统一接口域名，测试环境
 // 生产环境
-// const domain = "https://www.abigtree.top/eshop/api"
+const domain = "https://www.abigtree.top/eshop/api"
 // MD5加密
 const salt = "eshop-app"
 
-const picDomain = "https://www.abigtree.top/eshop/static/app/image"
+const picDomain = "https://www.abigtree.top/eshop/static/admin/image"
 
 
 module.exports = {
