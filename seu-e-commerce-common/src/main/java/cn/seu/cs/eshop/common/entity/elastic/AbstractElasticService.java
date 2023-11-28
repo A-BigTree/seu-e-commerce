968e1d0c3d132444d37ceda64e261fee22638ef6
Shuxin Wang <81904920+A-BigTree@users.noreply.github.com>
2023-11-28 14:53:00 +0800

    Update AbstractElasticService.java

--- seu-e-commerce-common/src/main/java/cn/seu/cs/eshop/common/entity/elastic/AbstractElasticService.java
+++ seu-e-commerce-common/src/main/java/cn/seu/cs/eshop/common/entity/elastic/AbstractElasticService.java
@@ -2,10 +2,16 @@ package cn.seu.cs.eshop.common.entity.elastic;
 
 import jakarta.annotation.Resource;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.data.elasticsearch.core.query.IndexQuery;
 import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
 /**
  * @author Shuxin Wang <shuxinwang662@gmail.com>
@@ -24,6 +30,17 @@ public abstract class AbstractElasticService<INDEX extends EsBaseIndex> {
                 .withObject(index)
                 .build();
         return elasticsearchTemplate.index(indexQuery, IndexCoordinates.of(getIndex()));
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<INDEX> search(Query query) {
+        SearchHits<INDEX> result = elasticsearchTemplate.search(query,
+                (Class<INDEX>) ((ParameterizedType) getClass()
+                .getGenericSuperclass()).getActualTypeArguments()[0],
+                IndexCoordinates.of(getIndex()));
+        return result.get()
+                .map(SearchHit::getContent)
+                .toList();
     }
 
     public abstract String getIndex();
